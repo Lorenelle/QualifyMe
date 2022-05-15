fea09617a211fa8eda9a1ef46a5a8a9a07f3efa4
Louela <louelabalili15@gmail.com>
2022-05-15 15:27:37 +0800

    Adding package into Repository

---
+++ QualifyMe.DomainModels/QualifyMeDatabaseDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualifyMe.DomainModels
+{
+    public class QualifyMeDatabaseDbContext
+    {
+        public DbSet<StudentUser> StudentUsers { get; set; }
+        public DbSet<CompanyUser> CompanyUsers { get; set; }
+        public DbSet<AdminUser> AdminUsers { get; set; }
+    }
+}
